a7b5b7584a1af8efa720c4740ad5b032495e9ba5
Jaime Navarrete <jaimenava123@outlook.com>
2021-03-09 22:21:13 -0600

    Agregados - Los 2 ejercicios de la guia 6

---
+++ Guia6/Codigo Ejercicio 1 y 2/app/src/main/java/com/udb/dsm/sextaapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.udb.dsm.sextaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio_2);
+//        setContentView(R.layout.ejercicio_1);
+//        setContentView(R.layout.ejemplo_grid_layout);
+//        setContentView(R.layout.ejemplo_table_layout);
+//        setContentView(R.layout.ejemplo_relative_layout);
+//        setContentView(R.layout.ejemplo_linear_layout);
+//        setContentView(R.layout.ejemplo_frame_layout);
+//        setContentView(R.layout.activity_main);
+    }
+}
